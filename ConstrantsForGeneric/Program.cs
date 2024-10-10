fea25c553a42f3ea10019edb67c50c2ec8c093d2
alif.meydi <alif.meydi@rds.co.id>
2024-10-10 10:26:49 +0700

    feat: first commit

---
+++ ConstrantsForGeneric/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConstrantsForGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car<RollsRoyce> rollsRoyce = new Car<RollsRoyce>(); // genericnya harus dimasukkin class, di class Car ada kondisi where
+            Car<string> bmw = new Car<string>(); // ini harusnya gabisa, soalnya genericnya diisi string, bukan class
+        }
+    }
+
+    class RollsRoyce
+    {
+        public int HorsePower { get; set; }
+        public int Model { get; set; }
+    }
+}
